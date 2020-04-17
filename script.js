1169acd5b9eed7e4ee4c02c61415868cd5a3550f
Student101 <student101@PSAdmins-Air-3.home>
2020-04-16 22:56:15 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+
+let i;
+
+// even numbers: 
+// for(i=1; i<=20; i+=1){
+//     if (i%2==0){
+//      document.getElementById('display').innerHTML += i + "<br>";
+//     }   
+//     }
+    
+
+// odd numbers: 
+//     for(i=1; i<=20; i+=1){
+//         if (i%2 !==0){
+//          document.getElementById('display').innerHTML += i + "<br>";
+//         }       
+//         }
+
+
+
+// prime numbers:
+        for(i=1; i<=20; i+=1){
+            if (i==1){
+                document.getElementById('display').innerHTML
+            }
+            if (i==2 || i==3 || i==5 || i==7){
+             document.getElementById('display').innerHTML += i + "<br>";
+            }
+            if (i%2!=0 && i%3!=0 && i%5!=0 && i%7!=0 ){
+                document.getElementById('display').innerHTML += i + "<br>";
+               } 
+        }
+            
+
+
